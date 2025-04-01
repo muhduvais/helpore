2a8cdae1d115b58237696d1cbb1ad9576b84b387
muhduvais <uvaisp110@gmail.com>
2025-04-01 20:14:24 +0530

    fix: bug fixes

--- backend/src/config/firebase.config.ts
+++ backend/src/config/firebase.config.ts
@@ -3,8 +3,14 @@ import { ServiceAccount } from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
 
+if (!process.env.GOOGLE_CREDENTIALS_JSON) {
+    throw new Error("GOOGLE_CREDENTIALS_JSON is not set in the environment variables");
+}
+
+const firebaseConfig: ServiceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON);
+
 admin.initializeApp({
-    credential: admin.credential.cert(process.env.GOOGLE_CREDENTIALS_JSON as ServiceAccount),
+    credential: admin.credential.cert(firebaseConfig),
 });
 
 export const firebaseAdmin = admin;
